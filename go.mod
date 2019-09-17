a3d65149461ef662f47210702250ad3fdbdbdea1
Elio Marcolino <eliom@jfrog.com>
2019-09-17 09:32:33 -0700

    New version without mod file

--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/elioengcomp/go-module-example-4
-
-require github.com/elioengcomp/go-module-example-2 v1.1.0
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/elioengcomp/go-module-example-5/tool"
+)
+
+func main() {
+	fmt.Printf("Exec: %s\n", tool.DoWork("test"))
+}
