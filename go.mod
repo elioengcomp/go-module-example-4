994b4f31e745218ca8f1024aedcdc88de4ccd1f0
Elio Marcolino <eliom@jfrog.com>
2019-09-18 10:40:29 -0700

    Added bad dependency

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/elioengcomp/go-module-example-4
+
+require (
+    git.apache.org/thrift.git v0.0.0-20150427210205-dc799ca07862
+)
+
+go 1.12
--- usetool/main.go
+++ usetool/main.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/elioengcomp/go-module-example-5/tool"
 )
 
 func main() {
-	fmt.Printf("Exec: %s\n", tool.DoWork("test"))
+	fmt.Printf("Exec: %s\n", "test")
 }
